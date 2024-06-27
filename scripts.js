ca2afbec91665458e084e56c0e80b731db6f972f
Cvitanovic <Robert.Cvitanovic@teilnehmer-bfwsb.de>
2024-06-27 11:24:04 +0200

    27.6. 11h, mobile responsive, not finished

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const nav = document.querySelector('.nav1');
+    nav.classList.toggle('show');
+}
+
+function closeMenu() {
+    const nav = document.querySelector('.nav1');
+    nav.classList.remove('show');
+}
